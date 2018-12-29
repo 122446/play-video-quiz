bdb8b6508c9b21ae322582262fca19267c058e3e
122446 <46224024+122446@users.noreply.github.com>
2018-12-29 22:11:20 +0700

    Create Video_Library.java

---
+++ Video_Library.java
@@ -0,0 +1,71 @@
+package application_video_quiz.com.videoquiz;
+
+public class Video_Library {
+    
+    
+    //////////I put in the Integer Array instead of mQuestion[]
+
+    private  Integer mVideos[]={
+            R.raw.vid1,
+            R.raw.vid2,
+            R.raw.vid3,
+            R.raw.vid4
+    };
+
+    /*private String mQuestions [] = {
+            "Which part of the plant holds it in the soil?",
+            "This part of the plant absorbs energy from the sun.",
+            "This part of the plant attracts bees, butterflies and hummingbirds.",
+            "The _______ holds the plant upright."
+
+    };*/
+
+
+    private String mChoices [][] = {
+            {"Roots", "Stem", "Flower"},
+            {"Fruit", "Leaves", "Seeds"},
+            {"Bark", "Flower", "Roots"},
+            {"Flower", "Leaves", "Stem"}
+    };
+
+
+
+    private String mCorrectAnswers[] = {"Roots", "Leaves", "Flower", "Stem"};
+
+
+
+
+   /* public String getQuestion(int a) {
+        String question = mQuestions[a];
+        return question;
+    }*/
+
+   ///////also i change it here
+    public Integer getQuestion(int a) {
+        Integer question = mVideos[a];
+        return question;
+    }
+
+
+    public String getChoice1(int a) {
+        String choice0 = mChoices[a][0];
+        return choice0;
+    }
+
+
+    public String getChoice2(int a) {
+        String choice1 = mChoices[a][1];
+        return choice1;
+    }
+
+    public String getChoice3(int a) {
+        String choice2 = mChoices[a][2];
+        return choice2;
+    }
+
+    public String getCorrectAnswer(int a) {
+        String answer = mCorrectAnswers[a];
+        return answer;
+    }
+
+}
